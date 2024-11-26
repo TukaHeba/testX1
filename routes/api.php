11f84e351466ba9abb9f7a9dcbd933b082f3511f
RimaKi <rimakiwan1999@gmail.com>
2024-11-26 23:42:58 +0300

    <api> routes/api : add test api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post("/welcome",function (){
+   return "Hi guys, I`m Rima Kiwan";
+});
