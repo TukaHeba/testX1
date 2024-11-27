02ca533193b64a82b54a1342a112f83845f6a01f
Mona <moon.2060@gmail.com>
2024-11-27 16:36:39 +0300

    " I am making sure if I can send mirge message from my side "

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view(' I hate branches !!!!');
+    return view(' I hate branches !!!! for the second and third and fourth time hahahaha ');
 });
