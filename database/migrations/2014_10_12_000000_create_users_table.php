2fbf089fee21465fac2c065e9d0fbfb332438e0e
Tuka Heba <tuka.heba@gmail.com>
2024-11-27 04:17:38 +0300

    test tuka46

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('password3');
             $table->string('password4');
             $table->string('password7');
+            $table->string('password7');
             $table->rememberToken();
             $table->timestamps();
         });
