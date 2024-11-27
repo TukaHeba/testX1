d0de440b9e43d3d01f965b779f22687850bf3248
Mona <moon.2060@gmail.com>
2024-11-27 16:47:11 +0300

    "adding other Route"

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view(' I hate branches !!!! for the second and third and fourth time hahahaha ');
 });
+
+Route::get('/About', function () {
+    return view(' Helllllllo From the Other siiiiiiiiiiiiide  ');
+});
